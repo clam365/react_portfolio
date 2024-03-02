bfd7abb8697213689a4db9eefc45a8be95d5f419
Christopher Lam <127339027+clam365@users.noreply.github.com>
2024-03-02 03:30:21 -0500

    Instead of using spline obj, we use its img form for loadup fast, no interaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Spline from '@splinetool/react-spline';
+import SplineBlob from "../src/assets/blobsSpline.png";
 import ProfileSection from "../src/Sections/ProfileSection/Profile";
 import "./App.css";
 import "./mediaqueries.css";
@@ -11,22 +11,22 @@ function App() {
     return (
         <body className={"relative overflow-x-hidden"}>
 
-            <div style={{
+            <div id="splineBlob" style={{
                 position: 'absolute',
-                top: 45,
-                left: -40,
+                top: 175,
+                left: 200,
                 width: '100%',
                 height: '100%',
                 zIndex: 0,
                 pointerEvents: 'none'
-            }} className={"scale-[50%] "}>
-                <Spline scene="https://prod.spline.design/kr3ccE52vUxCE3Bl/scene.splinecode"/>
+            }} >
+                <img src={SplineBlob} alt={""} className={"scale-90"}/>
             </div>
 
             <div className={"z-10"}>
                 <nav id={"desktop-nav"}
                      className={"bg-transparent flex justify-around items-center h-[17vh] text-black z-10"}>
-                    <div className={"text-[#635D5C] font-semibold text-[2rem] bg-transparent hover:cursor-default logo"}>CHRIS LAM
+                    <div className={"text-[#635D5C] font-semibold text-[2rem] bg-transparent hover:cursor-default logo profileSection"}>CHRIS LAM
                     </div>
                     <NavLinks/>
                 </nav>
--- src/Nav/NavLinks.tsx
+++ src/Nav/NavLinks.tsx
@@ -1,6 +1,6 @@
 export default function NavLinks() {
     return (
-        <div className={"flex bg-transparent"}>
+        <div className={"flex bg-transparent profileSection"}>
             <ul className={"flex gap-[2rem] list-none text-[1.5rem] nav-links bg-transparent nav-links"}>
                 <li className={"bg-transparent text-[#635D5C]"}>
                     <a href={"#about"}>ABOUT</a>
