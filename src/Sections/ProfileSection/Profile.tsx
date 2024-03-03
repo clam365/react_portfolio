be6960d17416ae47f3cd03a13e27fbb0a3f07f59
Christopher Lam <127339027+clam365@users.noreply.github.com>
2024-03-03 18:45:20 -0500

    About Page and Profile Page is now fully responsive

--- src/Sections/ProfileSection/Profile.tsx
+++ src/Sections/ProfileSection/Profile.tsx
@@ -1,27 +1,34 @@
 import ButtonContainer from "./ButtonContainer";
 import SocialsContainer from "./SocialsContainer";
-import "../../index.css"
-
+import "../../index.css";
 import React from "react";
 import SplineBackground from "../../Functions/SplineBackground";
+
 export default function Profile() {
     return (
-        <section id={"profile"} className={"flex justify-center gap-[5rem] h-[80vh] "}>
-            <div className={"z-0"}><SplineBackground/></div>
-            <div
-                className={"grid mt-[6rem] justify-center items-center text-center absolute transition-all profileSection"}>
-                <p className={"text-black font-semibold text-center m-[10px] text-[1.75rem] mb-[1rem]"}>Hello, I'm</p>
-                <h1 className={"text-black font-bold  text-[3rem] text-center -m-[15px]"}>Christopher Lam</h1>
-                <p className={"text-black font-semibold text-center m-[10px] text-[1.75rem] mt-[1.5rem]"}>Software
-                    Engineer</p>
+        <section id="profile" className="flex justify-center h-[80vh]">
+            <div className="z-0">
+                <SplineBackground />
+            </div>
+            <div className="grid mt-[6rem] mr-5 relative transition-all profileSection">
+                <div className={"profileText"}>
+                    <p className="text-black font-semibold text-center text-[1.75rem]">
+                        Hello, I'm
+                    </p>
+                    <h1 className="text-black font-bold text-[3rem] text-center">
+                        Christopher Lam
+                    </h1>
+                    <p className="text-black font-semibold text-center text-[1.75rem]">
+                        Software Engineer
+                    </p>
 
-                <div className={"grid mt-2 justify-center m-auto"}>
-                    <ButtonContainer/>
-                    <SocialsContainer/>
+                    <div className="grid mt-2 justify-center m-auto">
+                        <ButtonContainer/>
+                        <SocialsContainer/>
+                    </div>
                 </div>
 
             </div>
-
         </section>
     );
 }
